36b680abf2bc93f543a066628177e600468f6578
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-12-04 17:25:01 +0530

    Fix:Fixed the design bug

--- src/Components/TaskCard.jsx
+++ src/Components/TaskCard.jsx
@@ -1,69 +1,142 @@
-import React from 'react'
-import removeTask from '../utils/RemoveTask'
-import UserContext from '../utils/UserContext'
-import { useContext } from 'react'
-import comletedTask from '../utils/CompletedTask'
-import Addtask from '../utils/AddTask'
-import removeTaskCompleted from '../utils/RemoveTaskCompleted'
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 
+import removeTask from "../utils/RemoveTask";
+import UserContext from "../utils/UserContext";
+import { useContext, useEffect } from "react";
+import comletedTask from "../utils/CompletedTask";
+import Addtask from "../utils/AddTask";
+import removeTaskCompleted from "../utils/RemoveTaskCompleted";
+import "../App.css";
 const TaskCard = (props) => {
-    const {globalTigger,setglobalTigger} = useContext(UserContext);
-    const {globalTiggerCompleted,setglobalTiggerCompleted} = useContext(UserContext);
-    console.log("from task card gt: ",globalTigger);
-    console.log("from task card gtc:" , globalTiggerCompleted);
-    const removeTaskCard = (taskname) => {
-       
-       const n = removeTask(taskname);
-        setglobalTigger(!globalTigger);
-       return n; 
-    }
+  const { globalTigger, setglobalTigger } = useContext(UserContext);
+  const { globalTiggerCompleted, setglobalTiggerCompleted } =
+    useContext(UserContext);
+  console.log("from task card gt: ", globalTigger);
+  console.log("from task card gtc:", globalTiggerCompleted);
+  const removeTaskCard = (taskname) => {
+    const n = removeTask(taskname);
+    setglobalTigger(!globalTigger);
+    return n;
+  };
 
-const obj = {task:props.task,Priority:props.priority};
+  const obj = { task: props.task, Priority: props.priority };
+
+  const navMap = {
+    1: "red",
+    2: "blue",
+    3: "green",
+  };
+
+  const styleObj = {
+    width: "92%",
+  };
+
   return (
-    <div className="card w-[92%] bg-[#fff] h-[12vmin] flex">
-    <div className="completeholder w-[10%]  h-full flex justify-center items-center">
-        { props.type != "completed" && <div className="container flex justify-center items-center">
-            <div className="round" id={props.task}>
-            <input type="checkbox" className="c1" onClick={()=>{comletedTask(obj); removeTaskCard(props.task); setglobalTiggerCompleted(!globalTiggerCompleted); let inputs = document.querySelectorAll('.c1');
-            for (let i = 0; i < inputs.length; i++) {
-                inputs[i].checked = false;
-            }}} />
-            
+    <div className={navMap[props.priority]} style={styleObj}>
+      <div className="card w-[92%] h-[12vmin] flex">
+        <div className="completeholder w-[10%]  h-full flex justify-center items-center">
+          {props.type != "completed" && (
+            <div className="container flex justify-center items-center">
+              <div className="round" id={props.task}>
+                <input
+                  type="checkbox"
+                  className="c1"
+                  onClick={() => {
+                    comletedTask(obj);
+                    removeTaskCard(props.task);
+                    setglobalTiggerCompleted(!globalTiggerCompleted);
+                    let inputs = document.querySelectorAll(".c1");
+                    for (let i = 0; i < inputs.length; i++) {
+                      inputs[i].checked = false;
+                    }
+                  }}
+                />
+              </div>
+            </div>
+          )}
+          {props.type === "completed" && (
+            <div className="container ">
+              <div className="btnholder ml-5 ">
+                <div
+                  className=" text-[#fff] flex items-center "
+                  onClick={() => {
+                    Addtask(obj);
+                    removeTaskCompleted(props.task);
+                    setglobalTigger(!globalTigger);
+                    setglobalTiggerCompleted(!globalTiggerCompleted);
+                  }}
+                >
+                  <img
+                    className="mb-2"
+                    src="left-chevron.png"
+                    alt=""
+                    height={20}
+                    width={20}
+                  />
+                </div>
+              </div>
+            </div>
+          )}
         </div>
-        
-</div>
-}
-{ props.type === "completed" && <div className="container ">
-            <div className="btnholder ml-1 "><div className=' text-[#fff] ' onClick={()=>{ Addtask(obj); removeTaskCompleted(props.task);  setglobalTigger(!globalTigger); setglobalTiggerCompleted(!globalTiggerCompleted)}}>Revert
-                <img className='mb-5 ' src="left-chevron.png" alt="" height={25} width={25}/></div></div>
-        
-</div>
-}
-    </div>
-    <div className="textholder text-[#000000] pl-1 ml-1 w-[82%] ">
-
-        <div className="text" ><p>{props.task}</p></div>
-        <div className="flag  mt-1 h-[4vmin] w-full ">
-            {props.priority === "3" && <img src="pin.png"  className= 'max-w-full max-h-full object-contain' alt="" />}
-            {props.priority === "2" && <img src="flag.png"  className= 'max-w-full max-h-full object-contain' alt="" />}
-            {props.priority === "1" && <img src="red-flag.png"  className= 'max-w-full max-h-full object-contain' alt="" />}
-
+        <div className="textholder  pl-1 ml-1 w-[82%] ">
+          <div className="text flex items-center mt-2 lg:mt-6">
+            <p className="text-[3.55vmin] lg:text-[2.75vmin] font-medium">
+              {props.task}
+            </p>
+          </div>
+          <div className="flag  mt-1 h-[2.65vmin] w-full ">
+            {/* {props.priority === "3" && (
+              <img
+                src="pin.png"
+                className="max-w-full max-h-full object-contain"
+                alt=""
+              />
+            )} */}
+            {/* {props.priority === "2" && (
+            <img
+              src="flag.png"
+              className="max-w-full max-h-full object-contain"
+              alt=""
+            />
+          )}
+          {props.priority === "1" && (
+            <img
+              src="red-flag.png"
+              className="max-w-full max-h-full object-contain"
+              alt=""
+            />
+          )} */}
+          </div>
         </div>
+        {props.type != "completed" && (
+          <div className="removeholder flex items-center w-[5%]  mr-2 h-full ">
+            <img
+              src="delete.png"
+              className="max-w-full max-h-full object-contain"
+              alt=""
+              onClick={() => {
+                removeTaskCard(props.task);
+              }}
+            />
+          </div>
+        )}
+        {props.type == "completed" && (
+          <div className="removeholder w-[8%] p-2 h-full flex items-center mr-2">
+            <img
+              src="delete.png"
+              className="max-w-full max-h-full object-contain"
+              alt=""
+              onClick={() => {
+                removeTaskCompleted(props.task);
+                setglobalTiggerCompleted(!globalTiggerCompleted);
+              }}
+            />
+          </div>
+        )}
+      </div>
     </div>
-    {props.type != "completed" &&
-    <div className="removeholder w-[8%] mt-2 mr-1 h-full p-1">
-        <img src="delete.png" className='max-w-full max-h-full object-contain' alt="" onClick={()=>{removeTaskCard(props.task)}}/>
-    </div>
-    }
-     {props.type == "completed" &&
-    <div className="removeholder w-[8%] mt-2 mr-1 h-full p-1">
-        <img src="delete.png" className='max-w-full max-h-full object-contain' alt="" onClick={()=>{removeTaskCompleted(props.task); setglobalTiggerCompleted(!globalTiggerCompleted); }}/>
-    </div>
-    }
+  );
+};
 
-</div>
-
-  )
-}
-
-export default TaskCard
+export default TaskCard;
--- src/Components/ToDoComponent.jsx
+++ src/Components/ToDoComponent.jsx
@@ -1,23 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import Addtask from '../utils/AddTask.js';
-import Resetlist from '../utils/Resetlist.js';
-import TaskCard from './TaskCard.jsx';
-import { useContext } from 'react';
-import UserContext from '../utils/UserContext.js';
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+import Addtask from "../utils/AddTask.js";
+import Resetlist from "../utils/Resetlist.js";
+import TaskCard from "./TaskCard.jsx";
+import { useContext } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import UserContext from "../utils/UserContext.js";
 
 const ToDoComponent = () => {
-   const [tigger,setTigger]=useState(0);
-   const [data,setData] = useState([]);
-   const {globalTigger,setglobalTigger} = useContext(UserContext);
-   const {globalTiggerCompleted,setglobalTiggerCompleted} = useContext(UserContext);
-   console.log("global tigger is : " , globalTigger)
-    const addData=()=>{
-      const task=document.getElementById("task").value;
-      const Priority=document.getElementById("Priority").value;
+  toast.configure();
+  const [tigger, setTigger] = useState(0);
+  const [data, setData] = useState([]);
+  const { globalTigger, setglobalTigger } = useContext(UserContext);
+  const { globalTiggerCompleted, setglobalTiggerCompleted } =
+    useContext(UserContext);
+  console.log("global tigger is : ", globalTigger);
+  const addData = () => {
+    const task = document.getElementById("task").value;
+    const Priority = document.getElementById("Priority").value;
     //   alert(task+" "+Priority)
-      const data={
-        task,Priority
-      }
+
+    if (task === "") {
+      toast.error("Task can't be empty. ...", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    } else {
+      const data = {
+        task,
+        Priority,
+      };
       const n = Addtask(data);
       if (n) {
         // if (tigger != 1) {
@@ -28,85 +41,103 @@ const ToDoComponent = () => {
         // }
         setTigger(!tigger);
         setglobalTigger(!globalTigger);
-        
       }
-
-
     }
-    const fetchData = () => {
-        console.log("fetch data called");
-    
-        // Check if 'task' data is available in localStorage
-        if (localStorage.getItem("task")) {
-            const taskArray = JSON.parse(localStorage.getItem("task"));
-    
-            // Check if taskArray is a valid array
-            // if (Array.isArray(taskArray)) {
-            //     // Log fetched data
-            //     console.log("Fetched tasks:", taskArray);
-    
-            //       // Sort the tasks by priority (convert priority to number for correct sorting)
-            //     const sortedTasks = taskArray.sort((a, b) => {
-            //         const priorityA = parseInt(a[1], 10);  // Convert priority to number
-            //         const priorityB = parseInt(b[1], 10);  // Convert priority to number
-            //         return priorityA - priorityB;  // Sort by priority
-                    
-            // });
-            //     setData(sortedTasks);
-    
-            //     // Log the sorted tasks to verify
-            //     console.log("Sorted tasks by priority:", sortedTasks);
-    
-            //     // Set the sorted tasks to state
-                
-            // } else {
-            //     console.log("Invalid data format in localStorage.");
-            // }
-            setData(taskArray)
-        } else {
-            setData([]);
-        }
-    };
-    
-    
-    useEffect(()=>{
-        fetchData();
+  };
+  const fetchData = () => {
+    console.log("fetch data called");
 
-    },[tigger,globalTigger])
+    // Check if 'task' data is available in localStorage
+    if (localStorage.getItem("task")) {
+      const taskArray = JSON.parse(localStorage.getItem("task"));
+
+      // Check if taskArray is a valid array
+      // if (Array.isArray(taskArray)) {
+      //     // Log fetched data
+      //     console.log("Fetched tasks:", taskArray);
+
+      //       // Sort the tasks by priority (convert priority to number for correct sorting)
+      //     const sortedTasks = taskArray.sort((a, b) => {
+      //         const priorityA = parseInt(a[1], 10);  // Convert priority to number
+      //         const priorityB = parseInt(b[1], 10);  // Convert priority to number
+      //         return priorityA - priorityB;  // Sort by priority
+
+      // });
+      //     setData(sortedTasks);
+
+      //     // Log the sorted tasks to verify
+      //     console.log("Sorted tasks by priority:", sortedTasks);
+
+      //     // Set the sorted tasks to state
+
+      // } else {
+      //     console.log("Invalid data format in localStorage.");
+      // }
+      setData(taskArray);
+    } else {
+      setData([]);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [tigger, globalTigger]);
   return (
     <>
-    {console.log(data)}
-    <div>
-    <p className='mt-4 ml-3 font-bold text-[6vmin]'>Add Task</p>
-    <div className='w-[100vw] md:w-[50vw] text-[#000000] flex mt-10 ml-3'>
-          <input type="text" placeholder='Add Task...' className='p-2 h-[7vmin] w-[60%]' id="task"/>
-          <select name="Priority" id="Priority" className='h-[7vmin] bg-[#C0C0C0] w-[15%] md:w-[10%] pl-1'>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
+      {console.log(data)}
+      <div>
+        <p className="mt-4 ml-3 font-bold text-[6vmin]">Add Task</p>
+        <div className="w-[100vw] md:w-[50vw] text-[#000000] flex mt-10 ml-3">
+          <input
+            type="text"
+            placeholder="Add Task..."
+            className="p-2 h-[10vmin] lg:h-[7vmin] w-[60%]"
+            id="task"
+          />
+          <select
+            name="Priority"
+            id="Priority"
+            className="h-[10vmin] lg:h-[7vmin] bg-[#C0C0C0] w-[15%] md:w-[10%] pl-1"
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
           </select>
-          <div className="additionholder  bg-[#3d18c0] h-[7.2vmin] w-[22%] md:w-[20%]  text-white   " onClick={()=>
-                {
-                    addData()
-                    document.getElementById("task").value="";
-                }}>
-            <p className='text-center  mt-1 '>
-            Add</p>
-              
+          <div
+            className="additionholder  bg-[#3d18c0]  w-[19%] md:w-[20%] h-[10vmin] lg:h-[7vmin] text-white   "
+            onClick={() => {
+              addData();
+              document.getElementById("task").value = "";
+            }}
+          >
+            <p className="text-center  mt-3 text-[2.25vmin] ">Add</p>
           </div>
+        </div>
+        <div className="Taskcardholder flex flex-col gap-y-3 mt-3 ml-3">
+          {data.length > 0 ? (
+            data
+              .sort((a, b) => a[1] - b[1])
+              .map((val, ind) => <TaskCard task={val[0]} priority={val[1]} />)
+          ) : (
+            <div></div>
+          )}
+        </div>
+        <div className="Buttonholder">
+          <button
+            className="bg-[#ff0000] p-2 mt-3 ml-3 "
+            onClick={() => {
+              Resetlist();
+              setTigger(!tigger);
+              document.getElementById("task").value = "";
+              setglobalTiggerCompleted(!globalTiggerCompleted);
+            }}
+          >
+            Clear All
+          </button>
+        </div>
       </div>
-      <div className="Taskcardholder flex flex-col gap-y-3 mt-3 ml-3">
-      {data.length>0?data.sort((a,b)=>a[1]-b[1]).map((val,ind)=>( <TaskCard  task={val[0]} priority={val[1]}/> ) ):<div></div>}
-      </div>
-      <div className="Buttonholder">
-          
-        <button className='bg-[#fa5339] p-2 mt-3 ml-3 ' onClick={()=>{  Resetlist(); setTigger(!tigger); document.getElementById("task").value=""; setglobalTiggerCompleted(!globalTiggerCompleted);}}>Clear All</button>
-        
-      </div>
-      </div>
-      </>
-  )
-}
+    </>
+  );
+};
 
-export default ToDoComponent
-
+export default ToDoComponent;
